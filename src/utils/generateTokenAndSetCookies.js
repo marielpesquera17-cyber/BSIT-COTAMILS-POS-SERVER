a8647ef096fb30c0ef63dec9236180cb8a79aa0e
marielpesquera <marielpesquera17@gmail.com>
2026-07-14 14:40:16 +0800

    Fix the error

--- src/utils/generateTokenAndSetCookies.js
+++ src/utils/generateTokenAndSetCookies.js
@@ -4,11 +4,13 @@ import ENV from "./env.js";
 const generateTokenAndSetCookies = (userId, res) => {
   const token = jwt.sign({ userId }, ENV.jwt.secret, { expiresIn: "7d" });
 
+  const isProduction = ENV.server.node_env === "production";
+
   res.cookie("token", token, {
     httpOnly: true,
-    secure: ENV.server.node_env === "production",
-    sameSite: "strict",
-    maxAge: 7 * 24 * 60 * 60 * 1000,
+    secure: isProduction, // Must be true if sameSite is 'none'
+    sameSite: isProduction ? "none" : "lax", // 'none' allows cross-domain cookies on Vercel
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
   return token;
